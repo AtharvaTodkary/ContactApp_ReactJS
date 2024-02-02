ad0ba67f1d57a7deaba9aa3672f8ec8370800e39
Atharva Todkary <100767857+AtharvaTodkary@users.noreply.github.com>
2024-02-02 23:23:57 +0530

    Add files via upload

---
+++ src/contacts.js
@@ -0,0 +1,22 @@
+var contacts = [
+  {
+    name: "luffy",
+    img: "https://imgs.search.brave.com/TKFMHsr77tX5kpL-Ab4rGOr2ErKwUuTNmgfFaDhsWZE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvbHVm/ZnktMTI3MC14LTEy/NzAtcGljdHVyZS1x/ODAzNGQxNnkzM296/NXljLmpwZw",
+    phone: "+91 9076554565",
+    email: "luffy@onepiece.com",
+  },
+  {
+    name: "Zoro",
+    img: "https://imgs.search.brave.com/3PMkXGM2Ma1Yk4711g3Mq1f_BEvwjcoTIAOUPHzzzW4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzA3Lzdj/L2E4LzA3N2NhODhj/NWUyOGQ5YTVkYWJi/YjM3NzY2NzAxNmQx/LmpwZw",
+    phone: "+91 9076554877",
+    email: "zoro@onepiece.com",
+  },
+  {
+    name: "Sanji",
+    img: "https://imgs.search.brave.com/8f1MZ_eyZOonTrgCqM8JphsWayG0F8HV8i9Gsjlhi4k/rs:fit:500:0:0/g:ce/aHR0cDovL20uZ2V0/dHl3YWxscGFwZXJz/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMy8wNi9QZnAt/Vmluc21va2UtU2Fu/amkuanBn",
+    phone: "+91 9076576565",
+    email: "sanji@onepiece.com",
+  },
+];
+
+export default contacts;
